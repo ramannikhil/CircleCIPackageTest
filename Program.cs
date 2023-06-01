3a54624e18e7b78e3dfb889673ccd6ebf9597f25
Raman Nikhil <111501090+ramannikhil@users.noreply.github.com>
2023-06-01 14:07:55 +0530

    added new files

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CircleCIPackage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            List<int> list = new List<int>();
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("done");
+        }
+    }
+}
